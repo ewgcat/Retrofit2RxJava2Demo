220e7009b1966a41dc031a5a431b6b83d199316e
lishuaihua <850716183@qq.com>
2020-08-28 09:49:09 +0800

    kotlin

--- app/src/main/java/com/example/myapplication/App.kt
+++ app/src/main/java/com/example/myapplication/App.kt
@@ -1,16 +1,13 @@
-package com.example.myapplication;
+package com.example.myapplication
 
-import android.app.Application;
+import android.app.Application
+import com.lishuaihua.retrofit.net.client.RetrofitClient
+import okhttp3.OkHttpClient
 
-import com.lishuaihua.retrofit.net.client.RetrofitClient;
-
-import okhttp3.OkHttpClient;
-
-public class App extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        RetrofitClient.init(getApplicationContext(), "http://wechat.kai-men.cn/pmsSrv/api/api!gateway.action/",R.raw.fullchain,builder,BuildConfig.DEBUG);
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val builder = OkHttpClient.Builder()
+        RetrofitClient.init(applicationContext, "http://wechat.kai-men.cn/pmsSrv/api/api!gateway.action/", R.raw.fullchain, builder, BuildConfig.DEBUG)
     }
 }
--- app/src/main/java/com/example/myapplication/LoginApiService.kt
+++ app/src/main/java/com/example/myapplication/LoginApiService.kt
@@ -1,22 +1,13 @@
-package com.example.myapplication;
+package com.example.myapplication
 
+import io.reactivex.Observable
+import org.json.JSONObject
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
 
-
-import org.json.JSONObject;
-
-import io.reactivex.Observable;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.POST;
-
-
-
-
-public interface LoginApiService {
-
+interface LoginApiService {
     @FormUrlEncoded
     @POST("http://wechat.kai-men.cn/pmsSrv/api/api!gateway.action")
-    Observable<JSONObject> login(@Field("reqStr") String jsonStr);
-
-
+    fun login(@Field("reqStr") jsonStr: String?): Observable<JSONObject?>?
 }
